16503928e1e1e23f7a4d9c8c96629eb20754a81c
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2022-07-07 17:45:25 +0200

    base project

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 from handleMsg import Message
+from handleMsg import User
 import re
 
 API_KEY = "5442072729:AAFp2AboU1g6_VSi-yhPfFSagdCABphaA8s"
 
 updater = Updater(API_KEY,
 				use_context=True)
+
+user_list = []
 
 #function that read a file line by line and return a list of bad words
 def get_bad_words():
@@ -21,7 +24,6 @@ def get_bad_words():
 	return bad_words_list
 
 bad_words_list = get_bad_words()
-print (bad_words_list)
 
 def start(update: Update, context: CallbackContext):
 	user = update.message.from_user
@@ -40,13 +42,38 @@ def unknown(update: Update, context: CallbackContext):
 
 
 def unknown_text(update: Update, context: CallbackContext):
-	user = update.message.from_user
+	user = User(update.message.from_user.id, update.message.from_user.username)
 	msg = Message(update.message.text)
+	needAdd = True
+	for user_ in user_list:
+			if(user_.id == update.message.from_user.id):
+				needAdd = False
+				break
+	if(needAdd):
+		user_list.append(user)
+		print("Added user : " + user.__str__())
+
 	if(msg.isAttack(bad_words_list)):
-		update.message.reply_text("Sei un jolly @"+ user.username + "!")
+		#search a user in the list and add a bad word
+		for user_ in user_list:
+			if(user_.id == update.message.from_user.id):
+				user_.addBadWord()
+				print("Bad word added")
+				break
+		update.message.reply_text("Sei un jolly @"+ user.__str__() + "!")
 	else:
-		update.message.reply_text(
-		"Sei veramente un Gentleman @"+ user.username + "!")
+		isGentle = False
+		#search a user in the list and add a good word
+		for user_ in user_list:
+			if(user_.id == update.message.from_user.id):
+				user_.addGoodWord()
+				print("Good word added")
+				if(user_.n_goodWords % 5 == 0):
+					isGentle = True
+				break
+		if(isGentle):
+			update.message.reply_text(
+			"Sei veramente un Gentleman @"+ user.__str__() + "!")
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
--- handleMsg.py
+++ handleMsg.py
@@ -11,10 +11,31 @@ class Message:
     def isAttack(self,bad_words):
         msg = self.msg.lower()
         msg = msg.replace(" ", "")
-        print ("isattack")
-        print (bad_words)
+        
         for bad_ward in bad_words:
             if bad_ward in msg:
                 return True
 
         return False; 
+
+class User:
+    def __init__(self, id, username = None):
+        self.id = id
+        if(username == None):
+            self.username = "Unknown"
+        else:
+            self.username = username
+        self.n_badWords = 0
+        self.n_goodWords = 0
+
+    def __str__(self):
+        return self.username
+
+    def addBadWord(self):
+        self.n_badWords += 1
+        self.n_goodWords = 0
+    
+    def addGoodWord(self):
+        self.n_goodWords += 1
+
+    
